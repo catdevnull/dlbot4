4136365c8a9a7316ab8e92e64ce32dfd89575d07
Nulo <hi@nulo.in>
2023-01-02 23:30:01 -0300

    Instagram bot

---
+++ instagram/go.mod
@@ -0,0 +1,10 @@
+module nulo.in/dlbot/instagram
+
+go 1.19
+
+replace nulo.in/dlbot/common => ../common
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	nulo.in/dlbot/common v0.0.0-00010101000000-000000000000
+)
---
+++ instagram/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/url"
+
+	"nulo.in/dlbot/common"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func respond(bot *tgbotapi.BotAPI, update tgbotapi.Update, url *url.URL) common.Result {
+	if url.Hostname() != "instagram.com" && url.Hostname() != "www.instagram.com" {
+		return common.NotValid
+	}
+
+	log.Printf("Downloading %s", url.String())
+	lookup, err := Lookup(url.String())
+	if err != nil {
+		return common.HadError
+	}
+	log.Println(lookup)
+
+	res := tgbotapi.NewVideo(update.Message.Chat.ID, tgbotapi.FileURL(lookup.VideoUrl))
+	res.ReplyToMessageID = update.Message.MessageID
+	res.Caption = "@" + lookup.Author + ": " + lookup.Text
+	bot.Send(res)
+	return common.Uploaded
+}
+
+func main() {
+	common.Main(common.Config{Respond: respond})
+}
