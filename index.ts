1a374b10ad41f4db396a2c009c72db3ebfced1c0
Nulo <hi@nulo.in>
2025-03-01 21:33:39 -0300

    gc

--- index.ts
+++ index.ts
@@ -40,14 +40,16 @@ class Bot {
     });
 
     this.bot.on("message", (msg: TelegramBot.Message) => {
-      this.handleMessage(msg).catch((error: Error) => {
-        console.error("Error al manejar el mensaje:", error);
-        this.bot.sendMessage(
-          msg.chat.id,
-          `Hubo un error al manejar el mensaje.`,
-          { reply_to_message_id: msg.message_id }
-        );
-      });
+      this.handleMessage(msg)
+        .catch((error: Error) => {
+          console.error("Error al manejar el mensaje:", error);
+          this.bot.sendMessage(
+            msg.chat.id,
+            `Hubo un error al manejar el mensaje.`,
+            { reply_to_message_id: msg.message_id }
+          );
+        })
+        .finally(() => Bun.gc(false));
     });
     this.bot.on("inline_query", async (query: TelegramBot.InlineQuery) => {
       try {
