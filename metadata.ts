3b38eac3e6bf12129b63a6e529400ebf48e766bc
Nulo <hi@nulo.in>
2025-03-07 00:00:58 -0300

    pass proxy url

--- metadata.ts
+++ metadata.ts
@@ -1,11 +1,13 @@
 import * as cheerio from "cheerio";
 import { FAKE_USER_AGENT } from "./consts";
+import { fetch } from "bun";
 
 export async function getDescription(url: string) {
   try {
     const res = await fetch(url, {
       headers: { "User-Agent": FAKE_USER_AGENT },
-    });
+      proxy: process.env.PROXY_URL,
+    } as any);
     const html = await res.text();
     const $ = cheerio.load(html);
     return $("meta[name='description']").attr("content") || null;
