e1f6271a50afa0d7c9e258bb1891110c8be98d15
Nulo <hi@nulo.in>
2025-03-13 20:51:43 -0300

    arreglar getdescription

--- metadata.ts
+++ metadata.ts
@@ -1,23 +1,31 @@
 import * as cheerio from "cheerio";
-import { fetch, ProxyAgent } from "undici";
+import { execFile } from "node:child_process";
+import { promisify } from "node:util";
+
+const execFileAsync = promisify(execFile);
 
 const PROXY_URL = process.env["PROXY_URL"];
 
 export async function getDescription(url: string) {
-  let client: ProxyAgent | undefined;
-  if (PROXY_URL) {
-    client = new ProxyAgent(PROXY_URL);
-  }
   try {
-    const res = await fetch(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (compatible; bingbot/2.0 +http://www.bing.com/bingbot.htm)",
-      },
-      dispatcher: client,
-    });
-    const html = await res.text();
-    const $ = cheerio.load(html);
+    const proxyArgs = PROXY_URL ? ["-x", PROXY_URL] : [];
+    // https://perishablepress.com/list-all-user-agents-top-search-engines/
+    let userAgent =
+      "Mozilla/5.0 (compatible; bingbot/2.0 +http://www.bing.com/bingbot.htm)";
+    if (url.includes("tiktok.com")) {
+      userAgent =
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 8_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B411 Safari/600.1.4 (compatible; YandexBot/3.0; +http://yandex.com/bots)";
+    }
+
+    const userAgentArgs = ["-A", userAgent];
+    const html = await execFileAsync("curl", [
+      "-s",
+      ...proxyArgs,
+      ...userAgentArgs,
+      url,
+    ]);
+
+    const $ = cheerio.load(html.stdout);
     return $("meta[name='description']").attr("content") || null;
   } catch (e) {
     console.warn(`Failed to get description for ${url}:`, e);
